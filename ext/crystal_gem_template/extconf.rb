ca0edace2ac469e1071c0f97c03196e48130f95d
Joseph Johansen <jo.johansen@hotmail.com>
2020-05-06 22:53:47 +0100

    Readd makefile

--- ext/crystal_gem_template/extconf.rb
+++ ext/crystal_gem_template/extconf.rb
@@ -1,7 +1,9 @@
 require "mkmf"
-find_executable("crystal") or abort
-$srcs = Dir["./**/*.cr"]
-$OBJEXT = "cr"
-create_makefile("crystal_gem_template.bundle") do |predefined_config|
-  predefined_config << "\n../../lib/crystal_gem_template.bundle: ./src/crystal_gem_template.cr\n\tcrystal $< --link-flags \"-dynamic -bundle -Wl,-undefined,dynamic_lookup\" -o ../../lib/crystal_gem_template.bundle\n"
-end
+find_executable("crystal") or abort <<~ERR
+  You need crystal installed to use this gem.
+  Please check out https://crystal-lang.org/ for information on how to install it.
+ERR
+
+# # this is pretty horrible
+# def create_makefile(_,_ = nil);end
+# create_makefile nil
