ea05a732eca3ad401b5ec31d4fbca398446d514d
Joseph Johansen <jo.johansen@hotmail.com>
2020-05-06 22:42:36 +0100

    Add my instruction to predefined config;

--- ext/crystal_gem_template/extconf.rb
+++ ext/crystal_gem_template/extconf.rb
@@ -2,6 +2,6 @@ require "mkmf"
 find_executable("crystal") or abort
 $srcs = Dir["./**/*.cr"]
 $OBJEXT = "cr"
-create_makefile("crystal_gem_template.bundle") do |_predefined_config|
-  "\n../../lib/crystal_gem_template.bundle: ./src/crystal_gem_template.cr\n\tcrystal $< --link-flags \"-dynamic -bundle -Wl,-undefined,dynamic_lookup\" -o ../../lib/crystal_gem_template.bundle\n"
+create_makefile("crystal_gem_template.bundle") do |predefined_config|
+  predefined_config << "\n../../lib/crystal_gem_template.bundle: ./src/crystal_gem_template.cr\n\tcrystal $< --link-flags \"-dynamic -bundle -Wl,-undefined,dynamic_lookup\" -o ../../lib/crystal_gem_template.bundle\n"
 end
