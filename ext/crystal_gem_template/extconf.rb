ed867da66c55bdcbb7f30a10d8c8262ebdff230e
Joseph Johansen <jo.johansen@hotmail.com>
2020-05-07 09:36:03 +0100

    Remove patch

--- ext/crystal_gem_template/extconf.rb
+++ ext/crystal_gem_template/extconf.rb
@@ -3,7 +3,3 @@ find_executable("crystal") or abort <<~ERR
   You need crystal installed to use this gem.
   Please check out https://crystal-lang.org/ for information on how to install it.
 ERR
-
-# this is pretty horrible
-def create_makefile(_,_ = nil);end
-create_makefile nil
